2e877379ff1edd8e587e09933527a724571caa45
Quentin102 <159295703+Quentin102@users.noreply.github.com>
2025-04-02 11:42:18 +0200

    Create Install_AD.ps1

---
+++ Install_AD.ps1
@@ -0,0 +1,26 @@
+# Installation des rôles AD DS et outils de gestion
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+# Vérification de l’installation
+if ((Get-WindowsFeature -Name AD-Domain-Services).InstallState -ne "Installed") {
+    Write-Host "L'installation d'Active Directory a échoué !" -ForegroundColor Red
+    exit 1
+}
+
+# Promotion du serveur en contrôleur de domaine
+Install-ADDSForest `
+-DomainName "monentreprise.local" `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2016" `
+-ForestMode "Win2016" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$true `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
+# Redémarrage du serveur pour finaliser l’installation
+Write-Host "Redémarrage du serveur dans 10 secondes..."
+Start-Sleep -Seconds 10
+Restart-Computer -Force
